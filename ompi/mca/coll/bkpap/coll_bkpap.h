80537cf099461dae9d4c06fa5952fdda0b0f84da
bkitor <bkitor@gmail.com>
2021-12-26 13:29:57 -0500

    needed files

---
+++ ompi/mca/coll/bkpap/coll_bkpap.h
@@ -0,0 +1,5 @@
+#include "ompi_config.h"
+
+int mca_coll_bkpap_init_query()
+
+mca_coll_base_module_t* mca_coll_bkpap_comm_query(mca_coll_base_module_t *module, struct ompi_communicator_t *comm)
---
+++ ompi/mca/coll/bkpap/coll_bkpap_component.c
@@ -0,0 +1,3 @@
+const char *mca_coll_bkpap_component_version_string = 
+    "Open MPI cuda collective MCA component version " OMPI_VERSION;
+	
